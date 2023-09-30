6a3a0cbdf698a3d6cc6d6ea9e6389f9d77f2f1dd
SeiriRyu <56245152+RyuSeiri@users.noreply.github.com>
2023-09-30 21:27:26 +0900

    fix ui

--- js/script.js
+++ js/script.js
@@ -154,6 +154,7 @@ document.addEventListener("DOMContentLoaded", async function () {
   // 加载更多按钮点击事件
   const loadMoreBtn = document.getElementById("loadMoreBtn");
   loadMoreBtn.addEventListener("click", function () {
+    mobileCurrentPage++;
     const startIndex = (mobileCurrentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
     const pageImages = currentImages.slice(startIndex, endIndex);
@@ -196,9 +197,28 @@ document.addEventListener("DOMContentLoaded", async function () {
     });
 
     // 判断是否还有更多图片可以加载，如果没有，隐藏加载更多按钮
-    if (mobileCurrentPage * itemsPerPage >= pageImages.length) {
+    if (itemsPerPage > pageImages.length) {
       loadMoreBtn.style.display = "none";
     }
+  });
+
+  // 向上滚动按钮
+  const scrollTopBtn = document.getElementById("scrollTopBtn");
+  // 当用户滚动页面时，显示或隐藏返回顶部按钮
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+      scrollTopBtn.classList.add("show");
+    } else {
+      scrollTopBtn.classList.remove("show");
+    }
+  });
+
+  // 当用户点击返回顶部按钮时，滚动到页面顶部
+  scrollTopBtn.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // 平滑滚动
+    });
   });
 
   await loadLanguage();
@@ -269,7 +289,6 @@ async function loadLanguage() {
     console.log(e);
   }
 }
-
 
 /**
  * 显示翻页按钮
